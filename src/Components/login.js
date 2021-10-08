40cc50dd17bdb862d46e8df446cd6434450425c4
Suzaku <jcfababeir09@gmail.com>
2021-10-08 16:44:10 +0800

    initial setup of file validation

--- src/Components/login.js
+++ src/Components/login.js
@@ -8,6 +8,22 @@ import {
 	MDBInputGroupText 
 	} from "mdb-react-ui-kit";
 
+		function signIn(e) {
+		e.preventDefault();
+    let email = document.getElementById('email').value;
+		let pwd = document.getElementById('password').value;
+    let formData = JSON.parse(localStorage.getItem('formData')) || [];
+    let exist = formData.length && 
+    JSON.parse(localStorage.getItem('formData')).some(data => data.email.toLowerCase() === email && data.pwd.toLowerCase() === pwd);
+    if(!exist){
+        alert("Incorrect login credentials");
+    }
+    else{
+        alert("success");
+    }
+    
+}
+
 const Login = () => {
 
 
@@ -16,15 +32,15 @@ const Login = () => {
 			<MDBRow  className="d-flex justify-content-center mt-5 p-2">
 				<h1 className="text-center">Sample Login Page</h1>
 				<MDBCol className="col-md-6 shadow-3 p-3 rounded-3">
-					<form className="my-3">
+					<form className="my-3" onSubmit={(e) => signIn(e)}>
 						<h4 className="my-3">Please Login </h4>
 						<MDBInputGroup className='mb-3'>
 							<MDBInputGroupText noBorder>Email</MDBInputGroupText>
-							<MDBInputGroupElement name="email" className='rounded' id='basic-url' type='email' required/>
+							<MDBInputGroupElement name="email" id="email" className='rounded' type='email' required/>
 						</MDBInputGroup>
 						<MDBInputGroup className='mb-3'>
 							<MDBInputGroupText noBorder>Password</MDBInputGroupText>
-							<MDBInputGroupElement name="password"className='rounded' id='basic-url' type='password' required/>
+							<MDBInputGroupElement name="password" id='password' type='password' required/>
 						</MDBInputGroup>
 						<button type="submit" className="btn btn-primary">Sign In</button>
 					</form>
--- src/Components/signup.js
+++ src/Components/signup.js
@@ -1,10 +1,56 @@
 import { Link } from 'react-router-dom';
 
+function signUp (e) {
+	e.preventDefault();
+	let fname = document.getElementById('fname').value,
+			lname = document.getElementById('lname').value,
+			email = document.getElementById('email').value,
+			pwd = document.getElementById('pwd').value;
+
+	let formData = JSON.parse(localStorage.getItem('formData')) || [];
+
+	let exist = formData.length && 
+			JSON.parse(localStorage.getItem('formData')).some(data => 
+					data.fname.toLowerCase() === fname.toLowerCase() && 
+					data.lname.toLowerCase() === lname.toLowerCase()
+			);
+
+	if(!exist){
+			formData.push({ fname, lname, email, pwd });
+			localStorage.setItem('formData', JSON.stringify(formData));
+			document.querySelector('form').reset();
+			document.getElementById('fname').focus();
+			alert("Account Created.\n\nPlease Sign In using the link below.");
+	}
+	else{
+			alert("Ooopppssss... Duplicate found!!!\nYou have already sigjned up");
+	}
+}
+
+
 const Signup = () => {
 	return (
 		<>
-			<h1>Hello</h1>
 			<Link to="/">back</Link>
+			<h2>SIGNUP FORM</h2>
+			<form onSubmit={(e)=> signUp(e)}>
+					<div class="form-group">
+						<input type="text" name="fname" id="fname" placeholder="First Name" required/>
+					</div>
+					<div class="form-group">
+						<input type="text" name="lname" id="lname" placeholder="Last Name" required/>
+					</div>
+					<div class="form-group">
+						<input type="email" name="email" id="email" placeholder="Email Address" required/>
+					</div>
+					<div class="form-group">
+						<input type="password" name="pwd" id="pwd" placeholder="Password" required/>
+					</div>
+					<div class="form-group">
+						<button type="submit" >Sign Up</button>
+					</div>
+			</form>
+			<p>Already a member? <a href="/signin.html">Sign In</a></p>
 		</>
 	);
 };
