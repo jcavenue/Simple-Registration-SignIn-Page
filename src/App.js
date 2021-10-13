312476e322653a5d78b01284f7972077f4ea4bf8
Suzaku <jcfababeir09@gmail.com>
2021-10-13 22:15:32 +0800

    add route to dashboard

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './Components/login';
 import Signup from './Components/signup';
+import Dashboard from './Components/dashboard';
 
 function App() {
   return (
     <Router>
 			<Switch>
 				<Route path="/signup" component={Signup}/>
-				<Route exact path="/" component={Login}/>		
+				<Route path="/dashboard" component={Dashboard} />	
+				<Route exact path="/" component={Login}/>	
 			</Switch>
 		</Router>
   );
