c4dc07563269ebca139a340490938630012772fd
Suzaku <jcfababeir09@gmail.com>
2021-10-13 22:15:59 +0800

    add authentication for dashboard

--- src/Components/dashboard.js
+++ src/Components/dashboard.js
@@ -1,12 +1,21 @@
-import { withRouter, Redirect } from "react-router-dom";
+import { withRouter, Redirect, useLocation } from "react-router-dom";
 
-const Dashboard = ({isAuth}) => {
-	if(!isAuth){
-		return <Redirect to="/"/>
-	}
-	return (
-		<h1>Hello</h1>
-	);
+const Dashboard = () => {
+	const location = useLocation();
+	
+	try {
+		const { isAuth = false, userDetails = '' } = location.state;
+		
+		if(!isAuth){
+			return <Redirect to="/" />
+		}
+
+		return (
+		<h1>Hello {userDetails}</h1>
+		);
+	} catch(err){
+		return <Redirect to="/" />
+	}	
 }
 
 export default withRouter(Dashboard);
